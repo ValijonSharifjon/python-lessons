7463beb161f6623a890c4f136b72dc2e95d16c63
ValijonSharifjon <sharifjonovvalijon@gmail.com>
2022-12-15 08:16:21 +0500

    korzinka
    buurtma berish

---
+++ python.py
@@ -0,0 +1,49 @@
+mahsulot=[]
+print('Korzinkaga Hush Kelibsiz!')
+print('Nima buyurtma berasiz:')
+n=0
+while True:
+    n+=1
+    buyurma=input(f'{n}chi mahsulot>>>')
+    mahsulot.append(buyurma)
+    takror=input('Yana buyurtma qilasizmi (ha\yuq)')
+    if takror.lower() != 'ha':
+        break
+print('Buyurtma uchun raxmat')
+print('Buyurtma bergan mahsulotlariz ruyhati:')
+for m in mahsulot:
+    print(m.title())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+   
+
+
+
+        
+
+
+
+
+
+
+
+
+
